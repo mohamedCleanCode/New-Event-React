7e6fbee248a8f403653af3cc1d228e4ba971a38f
mohamed <m635379@gmail.com>
2022-12-29 10:11:26 +0200

    add font awesome to my project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      Test
+      <FontAwesomeIcon icon={faUser} />
+    </div>
+  );
 }
 
 export default App;
