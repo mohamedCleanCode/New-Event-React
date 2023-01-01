78e40039d6b2c4c6aa273e03d883f2531b10608e
mohamed <m635379@gmail.com>
2023-01-01 13:14:43 +0200

    install styled-component && npm-check-updates

--- src/design-system/components/OverView/components/Card.js
+++ src/design-system/components/OverView/components/Card.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function Card({ icon, title, desc }) {
   return (
-    <div className="card col-sm-6 col-lg-4">
+    <div className="over-view-card col-sm-6 col-lg-4">
       <FontAwesomeIcon icon={icon} />
       <h2>{title}</h2>
       <p>{desc}</p>
